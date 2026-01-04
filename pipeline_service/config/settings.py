7dfa39214f1b8510ee696e088cf24af0ede90d99
“mayabcdef” <mayabc.def.123@gmail.com>
2026-01-04 23:50:45 +0700

    change ckpt

--- pipeline_service/config/settings.py
+++ pipeline_service/config/settings.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
 
     # Trellis settings
     trellis_model_id: str = Field(default="jetx/trellis-image-large", env="TRELLIS_MODEL_ID")
-    trellis_sparse_structure_steps: int = Field(default=8, env="TRELLIS_SPARSE_STRUCTURE_STEPS")
+    trellis_sparse_structure_steps: int = Field(default=15, env="TRELLIS_SPARSE_STRUCTURE_STEPS")
     trellis_sparse_structure_cfg_strength: float = Field(default=5.75, env="TRELLIS_SPARSE_STRUCTURE_CFG_STRENGTH")
     trellis_slat_steps: int = Field(default=20, env="TRELLIS_SLAT_STEPS")
     trellis_slat_cfg_strength: float = Field(default=2.4, env="TRELLIS_SLAT_CFG_STRENGTH")
@@ -45,7 +45,7 @@ class Settings(BaseSettings):
     qwen_edit_prompt_path: Path = Field(default=config_dir.joinpath("qwen_edit_prompt.json"), env="QWEN_EDIT_PROMPT_PATH")
 
     # Backgorund removal settings
-    background_removal_model_id: str = Field(default="tuandao-zenai/rm_bg", env="BACKGROUND_REMOVAL_MODEL_ID")
+    background_removal_model_id: str = Field(default="yuvraj108c/RMBG-2.0", env="BACKGROUND_REMOVAL_MODEL_ID")
     input_image_size: tuple[int, int] = Field(default=(1024, 1024), env="INPUT_IMAGE_SIZE") # (height, width)
     output_image_size: tuple[int, int] = Field(default=(518, 518), env="OUTPUT_IMAGE_SIZE") # (height, width)
     padding_percentage: float = Field(default=0.2, env="PADDING_PERCENTAGE")
